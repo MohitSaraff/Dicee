fcfd57dea77760acb78daf5d5c1af5b99b31573e
Mohit <saraffmohit@gmail.com>
2022-11-29 15:06:19 +0530

    Add Roll Dice Button

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const img1 = document.querySelector(".img1");
 const img2 = document.querySelector(".img2");
 const h1 = document.querySelector("h1");
+const btn = document.querySelector(".btn");
 let randomNumber1 = Math.floor(Math.random() * 6) + 1;
 let randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
@@ -14,3 +15,5 @@ if (randomNumber1 > randomNumber2) {
 } else {
   h1.innerHTML = "Player 2 Wins! 🚩";
 }
+
+btn.addEventListener("click", () => location.reload())
